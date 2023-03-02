8fe475c61a5a0eded887117351284cf8bcda5cfb
LancelotP <lancelot@prigent.dev>
2023-03-02 13:52:49 +0100

    triger geoloc on pwa

--- src/About.tsx
+++ src/About.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const About: React.FC = () => (
-  <h1>This is a PWA</h1>
-);
+const About: React.FC = () => {
+  const onButtonClick = async () => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+      }, (error) => {
+        console.log(error)
+      });
+    } else {
+      alert('Geolocation not supported on this device');
+    }
+  }
+  
+  return (
+    <h1>
+      <div>This is a PWA</div>
+      <button onClick={onButtonClick}>Ask geoloc</button>
+    </h1>
+  );
+};
 
 export default About;
